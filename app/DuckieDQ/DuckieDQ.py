4f0b3bab074f00f93a19c8b079c179d60025b6fd
Kaboupi <gnovozhiloff@gmail.com>
2025-04-23 14:08:17 +0300

    update: parser add: duckiedq class

--- app/DuckieDQ/DuckieDQ.py
+++ app/DuckieDQ/DuckieDQ.py
@@ -1,4 +1,3 @@
-from .supporting.setup.logging_config import setup_logging
 from sqlalchemy import create_engine
 from typing import Optional, Dict, List, Tuple, Any
 import clickhouse_connect
@@ -7,12 +6,33 @@ import pandas as pd
 import psycopg2 as pg
 import yaml
 
-from .supporting.connectors import PostgresConnector, ClickHouseConnector
+from .supporting.setup.logging_config import setup_logging
+from .supporting.connection import Connection
+from .supporting.parser import DQParser
 
+setup_logging()
 
 class DuckieDQ:
-    def __init__(self, source: dict, target: dict) -> None:
-        pass
+    __version: str = 'v0.0.1'
     
-    def run(self):
-        pass
+    def __init__(self, config_path: str) -> None:
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.info(f'Version {self.version}')
+        
+        self.configs: List[Dict[str, Any]] = DQParser(config_path).parse()
+    
+    def execute_dq(self):
+        for config in self.configs:
+            for task_name, task_values in config.items():
+                self.logger.info(f"Start to perform '{task_name}' task.")
+                conn_dict = task_values['conn_dict']
+                conn_type = task_values['conn_type']
+                conn_query = task_values['conn_query']
+                
+                conn = Connection(conn_dict, conn_type)
+                conn.query(conn_query)
+    
+    
+    @property
+    def version(self) -> str:
+        return self.__version
--- app/main.py
+++ app/main.py
@@ -1,27 +1,6 @@
 from DuckieDQ.supporting.connection import Connection
-from DuckieDQ import DuckieDQ
+from DuckieDQ.DuckieDQ import DuckieDQ
 
 if __name__ == '__main__':
-    db_postgres_params = {
-        'host': 'localhost',
-        'port': '5445',
-        'database': 'postgres_db',
-        'schema': 'default',
-        'username': 'postgres_user',
-        'password': 'postgres_password',
-    }
-    conn = Connection(db_postgres_params, 'postgres')
-    conn.connect()
-    conn.query('SELECT * FROM pg_namespace;')
-    conn.close()
-    
-    db_clickhouse_params = {
-        'host': 'localhost',
-        'port': '8123',
-        'database': 'default',
-        'username': 'admin',
-        'password': 'admin',
-    }
-    conn_2 = Connection(db_clickhouse_params, 'clickhouse')
-    conn_2.query('SELECT * from system.query_log limit 1')
-    conn_2.query('SELECT 1;')
+    duckie = DuckieDQ('../dq_configs')
+    duckie.execute_dq()
