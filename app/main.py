a31b8b99f4dc0f50b49102c74f714081a15b3319
Kaboupi <gnovozhiloff@gmail.com>
2025-04-23 14:30:52 +0300

    add: argparse to main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 from DuckieDQ.DuckieDQ import DuckieDQ
 
+import argparse
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', default='/', help='directiry with YAML/YML configs for DQ')
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    duckie = DuckieDQ('../dq_configs')
+    args = parse_args()
+    path = args.path
+    
+    duckie = DuckieDQ(path)
     duckie.execute_dq()
